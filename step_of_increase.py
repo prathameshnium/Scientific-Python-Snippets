ebb0d93a97aa936fd4f9d7d8d176d0d6e82e1771
prathameshnium <117578336+prathameshnium@users.noreply.github.com>
2022-11-12 22:17:17 +0530

    Create step_of_increase.py
    contain more than one prg

---
+++ step_of_increase.py
@@ -0,0 +1,19 @@
+#print(np.arange(-10, 10, 0.5))
+a=np.arange(-10, 10, 0.5)
+l1=[]
+for x in a:
+  l1.append(x)
+
+print(l1)
+
+-------------------------------------------
+
+#jugad
+
+I=10
+import math
+Inc=0.5
+
+div=(2*I)/(Inc)
+np.linspace(-I,I,math.ceil(div))
+
