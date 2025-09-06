c49ee4f49a31fe9061cc3a147718e4e5f67780d5
Prathamesh K. Deshmukh <117578336+prathameshnium@users.noreply.github.com>
2025-09-07 01:45:23 +0530

    Update Lab_Interfacing_supplimentry_old.py

--- Lab_Interfacing_supplimentry_old.py
+++ Lab_Interfacing_supplimentry_old.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-import numpy
+import numpy as np
 
 import os
 
@@ -10,20 +10,48 @@ import matplotlib.pyplot as plt
 
 
 I_range = float(input("Enter value of I: "))
-I_div = int(input("Enter division: "))
+I_step= float(input("Enter steps: "))
 filename = input("Enter filename: ")
 
+
+
+
+
 #-------------------------------------
+Volt=[]
 #I=np.linspace(-I_range, I_range,I_div) #dummy
 #V=np.linspace(0,0,I_div) #dummy 
-I=np.linspace(-I_range,I_range,I_div) #dummy
-V=np.linspace(0,0,I_div) #dummy 
+#I=np.linspace(-I_range,I_range,I_div) #dummy
+#V=np.linspace(0,0,I_div) #dummy 
+I=np.arange(-I_range,I_range,I_step)
+#V=np.arange(-I_range,I_range,I_step)
+dummy_step=int((2*I_range)/I_step)
+#V=np.linspace(0,10,num=dummy_step)
+V=np.linspace(0,10,num=dummy_step)
 
+
+#V=np.arange(0,0,dummy_step)
+#print(I)
+
+
+
+
+for ind in np.arange(-I_range,I_range,I_step):
+    Volt.append(ind*100)
+    
+
+
+#V1 = np.array(V)
+#print(I_step)
+#print(dummy_step)
 
 #--------------------------------
 
-plt.plot(I, V, marker='o', linestyle='-', color='g', 
-label='Square') 
+
+print(res)
+#--------------------------------
+
+plt.plot(I, Volt, marker='o', linestyle='-', color='g', label='Square') 
 plt.xlabel('I')
 plt.ylabel('V') 
 plt.title('IV curve')
@@ -40,4 +68,3 @@ df=pd.DataFrame()
 df['I']=pd.DataFrame(I)
 df['V']=pd.DataFrame(V)
 df.to_csv(r'C:/Users/ketan/OneDrive/Desktop/IV_files/IV_data_at_RT_'+str(filename), index=None, sep='	', mode='w')
-
